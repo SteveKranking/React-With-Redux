21ea02b45031b0b609e6eb649c6ba59d2fe60b1e
Stephen Kranking <stephenkranking@yahoo.com>
2018-04-02 12:57:20 -0400

    Errors that are bothering me

--- ReduxSimpleStarter/src/containers/books-list.js
+++ ReduxSimpleStarter/src/containers/books-list.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
  
     renderList() {
-            this.props.books.map((book) => {
-                return (
-                    <li key={book.title} className="list-group-item"> {book.title} </li>   
-                );
-            });
+        this.props.books.map((book) => {
+            return (
+                <li key={book.title} className="list-group-item"> {book.title} </li>   
+            );
+        });
     }
 
     render() {
@@ -18,3 +19,13 @@ export default class BookList extends Component {
         )
     }
 }
+
+function mapStateToProps(state) {
+    // Whatever is returned will shower up as props
+    // inside of BookList
+    return {
+        books: state.books
+    };  
+}
+
+export default connect(mapStateToProps)(BookList);
