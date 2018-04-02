df192396b2b770a06a513ff40b91b53a8127ff6d
Stephen Kranking <stephenkranking@yahoo.com>
2018-04-02 12:03:00 -0400

    first commit

---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: The good parts'},
+        { title: 'Harry Potter: Whichever you want'},
+        { title: 'Why the Packers are the best'},
+        { title: 'Programming for dummies'}
+    ]
+}
